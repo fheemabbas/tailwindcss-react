9e26cac8022924de12b9560fbf39d8e719b04f28
fahim <fahim@nimblechapps.com>
2022-03-30 09:27:27 +0530

    tailwindcss setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import './style/main.css'
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="h-64" >
+      <div className='p-4 m-4 bg-green-600' >
+        <h1 className='text-2xl font-bold text-white' > Hello Welcome to Tailwind CSS</h1 >
+      </div >
+    </div >
   );
 }
 
